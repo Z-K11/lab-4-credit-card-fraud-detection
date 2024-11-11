e6db2053928cb3bd1b83819f9d6e4481ceceb3c6
Z-K11 <zulqarnainbazidkhail@gmail.com>
2024-11-11 23:35:36 +0500

    Read Csv data from the file

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import normalize,StandardScaler
 from sklearn.utils.class_weight import compute_sample_weight
 from sklearn.metrics import roc_auc_score
 # data link
-url= "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/creditcard.csv"
-raw_data = pd.read_csv(url)
+#url= "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/creditcard.csv"
+raw_data = pd.read_csv("creditcard.csv")
 print("There are " + str(len(raw_data)) + " observations in the credit card fraud dataset.")
 print("There are " + str(len(raw_data.columns)) + " variables in the dataset.")
