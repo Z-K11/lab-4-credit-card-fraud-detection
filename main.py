d116380ab37a7c763a52a01b2529fd6f0bca172b
Z-K11 <zulqarnainbazidkhail@gmail.com>
2024-11-11 23:35:04 +0500

    Obtain csv data

---
+++ main.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import time
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import normalize,StandardScaler
+from sklearn.utils.class_weight import compute_sample_weight
+from sklearn.metrics import roc_auc_score
+# data link
+url= "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/creditcard.csv"
+raw_data = pd.read_csv(url)
+print("There are " + str(len(raw_data)) + " observations in the credit card fraud dataset.")
+print("There are " + str(len(raw_data.columns)) + " variables in the dataset.")
