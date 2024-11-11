b9396da172b36e31da3bdf4743d41fb477c07ce4
Z-K11 <zulqarnainbazidkhail@gmail.com>
2024-11-12 02:38:51 +0500

    Initial Commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# This is  a project to create a machine learning model for fast credit card fraud detection 
 from __future__ import print_function
 import matplotlib.pyplot as plt
 import time
@@ -12,3 +13,6 @@ from sklearn.metrics import roc_auc_score
 raw_data = pd.read_csv("creditcard.csv")
 print("There are " + str(len(raw_data)) + " observations in the credit card fraud dataset.")
 print("There are " + str(len(raw_data.columns)) + " variables in the dataset.")
+# Our data set has 284807 observations but in a real scenario a bank usually would have a larger data set compared to ours ?
+# To simulate such an experience we will inflate our data set with obersvations making it size 10 times bigger than the original
+multiplier = 10
